eea181f5de8a7b5b089c45ef6e29f088beef3ed4
kajkowsc <kajkowsc@tcd.ie>
2025-10-07 14:13:50 +0100

    continued work on PS1

--- my_answers/PS01.R_Kajkowski.R
+++ my_answers/PS01.R_Kajkowski.R
@@ -48,12 +48,11 @@ confint_90 <- c(lower_90, upper_90)
 #We are 90% confident that the true population mean of student IQ in the school lie around 93.96 and 102.92. 
 
 # part 2: hypothesis test
-#Ho = 100
+#Ho <= 100
 #Ha > 100
 t.test(student_iqs, mu = 100, alternative = "greater")
-print(critical_t <- qt(1 - 0.05, 24))
+#For this hypothesis test, the null hypothesis is that the population average of student IQ is 100 or lower. However, the alternative hypothesis states that the average is higher than 100. Therefore, this question is looking for a one sided t test, testing if there is sufficient evidence to reject the null. Using the information about the sample that was found from the part above (mean and standard deviation), the t statistic can be calculated with this equation: t = (sample mean - hypothesized population mean)/(standard deviation/square root of the sample size) = (98.44 - 100) / (13.093/√25). In order to discover if the null hypothesis can be rejected, we need to find the p value. In the code above, it gives us the value. To find it outside of coding, you need the degrees of freedom and t table to look up a range of scores the t value falls under. If that range is less than the alpha, the one would reject the null and vise versa one would fail to reject the null. In this problem, the calculated p value is 0.7215 is higher than our alpha of 0.05, therefore, we fail to reject the null hypothesis and there is insufficient evidence to prove the alternative hypothesis. 
 
-#For this hypothesis test, the null hypothesis is that the population average of student IQ is 100. However, the alternative hypothesis states that the average is higher than 100. Therefore, this question is looking for a one sided t test, testing if there is sufficient evidence to reject the null. Using the information about the sample that was found from the part above (mean and standard deviation), the t statistic can be calculated with this equation: t = (sample mean - hypothesized population mean)/(standard deviation/square root of the sample size). In order to discover if the null hypothesis can be rejected the calculated t value must be compared to a critical t value. In this case, the code above shows how to find the the critical t value in r. In order to find the critical t value three things are needed: type of test, alpha value, and the degrees of freedom. Since this test is a one sided test, with the alternative hypothesis stating the true population mean is larger than the hypothesized mean, the problem is looking at the upper tail, which is why the code shows 1 - 0.05 (alpha value). In this problem the critical value (1.712) is greater than the calculated t value (-0.596) therefore we fail to reject the null hypothesis and there is insufficient evidence to support the alternative hypothesis. 
 #####################
 # Problem 2
 #####################
@@ -102,10 +101,12 @@ plot(expenditure$X2,expenditure$X3,
 
 dev.off()
 
-#For the Y_X1 graph, the data is looking at the relationship between per capita expenditure on shelter/housing assistance and per capita personal income in state. The graph shows that as the personal income increases, so does the expenditure on shelter and housing assistance, therefore, we can say there is a correlation. However, the points aren't perfectly lined up so, the correlation is weak to moderate. 
-#For the Y_X2 graph, the data is looking at the relationship between per capita expenditure on shelter/housing assistance and number of residents that are "financially insecure" per 100,000 residents. The graph shows that as "finacially insecure" rsidents rise by 100,000, expenditure doesn't always increase as well. In certain areas were the expenditure is 300 and above there is more of a correlation, but lower than that the correlation is more scattered. 
-#For the Y_X3 graph, 
-
+#Y_X1: the data is looking at the relationship between per capita expenditure on shelter/housing assistance and per capita personal income in state. The graph shows that as the personal income increases, so does the expenditure on shelter and housing assistance, therefore, we can say there is a correlation. However, the points aren't perfectly lined up so, the correlation is weak to moderate. 
+#For the Y_X2 graph, the data is looking at the relationship between per capita expenditure on shelter/housing assistance and number of residents that are "financially insecure" per 100,000 residents. The graph shows that as "financially insecure" residents rise by 100,000, expenditure doesn't always increase as well. In certain areas were the expenditure is 300 and above there is more of a correlation, but lower than that the correlation is more scattered. 
+#For the Y_X3 graph, the data is looking at the relationship between per capita expenditure on shelter/housing assistance and number of people per thousand residing in urban areas in state. The graph shows that as the number of people residing increases, the expenditure also increases. The scatter plot shows a weak to moderate positive linear correlation. 
+#For the X1_X2 graph, 
+#For the X1_X3 graph,
+#For the X2_X3 graph, 
 
 #part 2: 
 
@@ -115,13 +116,9 @@ boxplot(Y ~ Region, data = expenditure,
         xlab = "Region (1=Northeast, 2=North Central, 3=South, 4=West)",
         ylab = "Y: Housing Expenditure per Capita")
 
-#EXPLAIN
+#For this question, the best way to visualize the different averages for each region is by using box plots. By looking at the box plots, the West has the highest per capita expenditure on housing/shelter assistance. By looking at the dark line which symbolizes the mean/average, the West's mean is around 85, which is higher than the other regions' average values. 
       
 #part 3: plot the relationship between Y and X1
-
-regions <- unique(expenditure$Region)
-colors <- c("blue","red","green","orange")[1:length(regions)]
-region_colors <- colors[as.numeric(expenditure$Region)]
   
 plot(expenditure$Y,expenditure$X1,
      main = "Relationship between Expenditure on shelters/hosing assistance 
@@ -129,13 +126,28 @@ plot(expenditure$Y,expenditure$X1,
      ylab = "Expenditure per capita",
      xlab = "Personal income per capita"
 )
+
+regions <- unique(expenditure$Region)
+colors <- c("forestgreen","skyblue","salmon","rosybrown")[1:length(regions)]
+shapes <- c(16, 17, 18, 15)[1:length(regions)]
+point_colors <- colors[match(expenditure$Region, regions)]
+point_shapes <- shapes[match(expenditure$Region, regions)]
      
 plot(expenditure$Y,expenditure$X1,
-     col = 
+     col = point_colors,
+     pch = point_shapes,
      main = "Relationship between Expenditure on shelters/hosing assistance 
      and Personal income in state",
      ylab = "Expenditure per capita",
      xlab = "Personal income per capita"
 )
+legend(
+  "bottomright", 
+  legend = region_names, 
+  col = colors, 
+  pch = shapes,
+  inset = 0.0
+  )
 
+#The relationship between per capita expenditure on shelter/housing assistance and per capita personal income per state shows a weak to moderate postivie linear relationship. Meaning, as the personal income increase, the expenditure on housing/shelter assistance also tend to increase 
       
